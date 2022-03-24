16d93239ff477c9b39a1b04f1d082853a18f8406
cantlookback <zinakov22@gmail.com>
2022-03-24 17:13:38 +0700

    done 2nd lab

--- lab2/lab2_1.c
+++ lab2/lab2_1.c
@@ -34,6 +34,61 @@ void insert(struct Trie *core, char *str){
     current->isLeaf = 1;
 }
 
+// Returns 1 if a given Trie node has any children
+int hasChildren(struct Trie* curr){
+    for (int i = 0; i < CHAR_SIZE; i++){
+        if (curr->character[i]) {
+            return 1;
+        }
+    }
+ 
+    return 0;
+}
+
+int deletion(struct Trie **curr, char* str){
+    // return 0 if Trie is empty
+    if (*curr == NULL) {
+        return 0;
+    }
+ 
+    // if the end of the string is not reached
+    if (*str){
+        // recur for the node corresponding to the next character in
+        // the string and if it returns 1, delete the current node
+        // (if it is non-leaf)
+        if (*curr != NULL && (*curr)->character[*str - 'a'] != NULL &&
+            deletion(&((*curr)->character[*str - 'a']), str + 1) &&
+                (*curr)->isLeaf == 0){
+            if (!hasChildren(*curr)){
+                free(*curr);
+                (*curr) = NULL;
+                return 1;
+            }
+            else {
+                return 0;
+            }
+        }
+    }
+ 
+    // if the end of the string is reached
+    if (*str == '\0' && (*curr)->isLeaf){
+        // if the current node is a leaf node and doesn't have any children
+        if (!hasChildren(*curr)){
+            free(*curr);    // delete the current node
+            (*curr) = NULL;
+            return 1;       // delete the non-leaf parent nodes
+        }
+ 
+        // if the current node is a leaf node and has children
+        else {
+            // mark the current node as a non-leaf node (DON'T DELETE IT)
+            (*curr)->isLeaf = 0;
+            return 0;       // don't delete its parent nodes
+        }
+    }
+ 
+    return 0;
+}
 
 int search(struct Trie *core, char *str){
     if (core == NULL){
@@ -56,6 +111,7 @@ int search(struct Trie *core, char *str){
 int main(){
 
     struct Trie *core = getNewNode();
+    
 
     return 0;
 }
---
+++ lab2/lab2_2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int N;
+    
+    cin >> N;
+    
+    cout << N - 1;
+
+    return 0;
+}
