d9f043256fd6204d955139fda010a426af119f95
cantlookback <zinakov22@gmail.com>
2022-04-11 09:54:50 +0700

    3rd lab done

---
+++ lab3/things.js
@@ -0,0 +1,12 @@
+function bibika(){
+    alert('BIBIKA');
+}
+
+function displayToggle() {
+    let x = document.getElementById("tog");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
